a818b152b4b40803395be811832d8cecdecdde39
Francesca DellAnno <francesca@dellanno.co.uk>
2024-09-08 21:26:08 +1000

    delete hooks file

--- src/pages/DiaryEntry.tsx
+++ src/pages/DiaryEntry.tsx
@@ -25,7 +25,7 @@ const DiaryEntry: React.FC = () => {
           <Button arrow text="Back" path="/diary-entries" />
         </ButtonWrapper>
         {loading && <Spinner text="Loading diary entry..." />}
-        {!diaryEntry && (
+        {!loading && !diaryEntry && (
           <Card>
             Uh oh! It looks like the diary entry you were looking for doesn't
             exist.
